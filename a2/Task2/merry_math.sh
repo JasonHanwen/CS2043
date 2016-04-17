5382dc0604f21e2d951fe9333f6b1ae62253fc8e
JasonHanwen <hw544@cornell.edu>
2016-04-17 17:26:11 -0400

    working on csv file

--- a2/Task2/merry_math.sh
+++ a2/Task2/merry_math.sh
@@ -2,9 +2,9 @@
 function usage() {
     echo "Usage:"
     echo "  - Argument 1: the name of file that you want to process"
-    echo "  - Argument 2: the operation to the file" add sub mul div operations
-    echo "  - Argument 3: the maximum length of the line"
-    echo "  - Argument 4: the minimum length of the line"
+    echo "  - Argument 2: the operation to the file including add sub mul div"
+    echo "  - Argument 3: the columns that you want to process"
+    echo " ...... "
 }
 
 if [ "$#" -lt 3 ]; then
@@ -12,44 +12,43 @@ if [ "$#" -lt 3 ]; then
   exit 64
 fi
 
-FileName = "$1"
+FileName="$1"
 
 if [ ! -f $FileName ]; then
     echo "the file does not exist"
     exit 64
 fi
 
-operation = "$2"
+operation="$2"
 touch RESULT.txt
 
-if [ $operation -ne add] && [ $operation -ne sub] && [ $operation -ne mul] && [ $operation -ne div] ; then
+if [ $operation -ne add ] && [ $operation -ne sub ] && [ $operation -ne mul ] && [ $operation -ne div ]; then
     echo the operation is not valid
     exit 1
-if
-num= 1;
+fi
 
-cat "$1" while read line
+cat $FileName | while IFS=, read col1 col2
 do
-  echo "Line is :" $line
-done < RESULT.txt
-
-
-while read line; do
-  IN="$line"
-  IFS=',' read -ra ADDR <<< "$IN"
-  l="${ADDR[$3-1]}"
-  for i in ${@:4} ; do
-    r="${ADDR[$i-1]}"
-    case "$2" in
-       "add") l="$(($l+$r))"
-       ;;
-       "sub") l="$(($l-$r))"
-       ;;
-       "mul") l="$(($l*$r))"
-       ;;
-       "div") l="$(($l/$r))"
-       ;;
-    esac
-  done
-  echo "$l" >> RESULTS.txt
+  echo "I got:$col1|$col2"
 done
+# done < RESULT.txt
+
+# while read line; do
+#   IN="$line"
+#   IFS=',' read -ra ADDR <<< "$IN"
+#   l="${ADDR[$3-1]}"
+#   for i in ${@:4} ; do
+#     r="${ADDR[$i-1]}"
+#     case "$2" in
+#        "add") l="$(($l+$r))"
+#        ;;
+#        "sub") l="$(($l-$r))"
+#        ;;
+#        "mul") l="$(($l*$r))"
+#        ;;
+#        "div") l="$(($l/$r))"
+#        ;;
+#     esac
+#   done
+#   echo "$l" >> RESULT.txt
+# done
